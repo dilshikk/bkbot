6e6209df6c7deefebed79f4f5a73178afdc33fb7
Dilshit <dilshikk@mail.ru>
2026-08-24 09:23:56 +0500

    feat: register user app router

--- bot/handlers/user/__init__.py
+++ bot/handlers/user/__init__.py
@@ -1,8 +1,9 @@
-﻿from aiogram import Router
+from aiogram import Router
 
-from bot.handlers.user import start, menu, mirror
+from bot.handlers.user import start, menu, mirror, app
 
 router = Router(name="user")
 router.include_router(start.router)
 router.include_router(menu.router)
 router.include_router(mirror.router)
+router.include_router(app.router)
